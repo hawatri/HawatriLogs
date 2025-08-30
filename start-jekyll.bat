2d02094f524fde3ec438558a1fb5c2d7b189a652
hawatri <fardinrahaman28@gmail.com>
2025-08-30 14:50:04 +0530

    Only github pages push

--- start-jekyll.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Jekyll development server...
-echo.
-echo Your site will be available at: http://localhost:4000
-echo Press Ctrl+C to stop the server
-echo.
-jekyll serve --host 0.0.0.0
-pause
